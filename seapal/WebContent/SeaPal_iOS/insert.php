b76533aae8680d44d499f1744ae61e28b85a12fc
unknown <Armin@.(none)>
2012-11-11 21:46:25 +0100

    .

--- seapal/WebContent/SeaPal_iOS/insert.php
+++ seapal/WebContent/SeaPal_iOS/insert.php
@@ -33,6 +33,7 @@
 		$w = $_POST[Genuagroesse];
 		$x = $_POST[Spigroesse];
 
+
 		$insert = mysql_query("Insert into binfos (Bootsname, Registernr, Segelzeichen, Heimathafen, Yachtclub, Eigner,	Versicherung, Rufzeichen, Typ, Konstrukteur, Laenge, Breite, Tiefgang, Masthoehe, Verdraengung, Rigart,	Bauhjahr, Motor, Tankgroesse, Wassertankgroesse, Abwassertankgroesse, Grosssegelgroesse, Genuagroesse, Spigroesse) values ('$a', '$b', '$c', '$d', '$e', '$f', '$g', '$h', '$i', '$j', '$k', '$l', '$m', '$n', '$o', '$p', '$q', '$r', '$s', '$t', '$u', '$v', '$w', '$x')");
 
 		mysql_close($con);
