2e47a3c9bd4f4d07836200db1240503bb261d777
unknown <arengess@3DLABOR13.in.fhkn.de>
2012-11-29 11:15:31 +0100

    Struktur verbessert
    __
    Armin

--- seapal/WebContent/SeaPal/footer.php
+++
@@ -1,5 +0,0 @@
-<div id="footer">
-	<p>
-		© 2012 IBN Verlag |<a href="http://seapal.de/impressum.php">Impressum</a> | <a href="http://seapal.de/legal.php">Legal notice</a> | SeaPal in <a href="http://itunes.apple.com/us/app/seapal/id561903907?mt=8&ign-mpt=uo%3D4">iTunes</a> | iPhone App Store | <a href="http://itunes.apple.com/us/app/seapal/id561903907?mt=8&uo=4" target="itunes_store"> iPad App Store</a> | Follow us on our <a href="https://plus.google.com/100825752167882077539/posts">Google Plus Page</a>
-	</p>
-</div>
