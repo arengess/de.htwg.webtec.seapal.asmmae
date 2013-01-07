fdf900c4122cd7898cf3b6dbe42e196667a1b707
micmeist <micmeist@htwg-konstanz.de>
2013-01-07 20:11:44 +0100

    better BootInfo.js! Worked on TripInfo.jsp

--- SeapalJSP/WebContent/js/bootInfo.js
+++ SeapalJSP/WebContent/js/bootInfo.js
@@ -1,4 +1,3 @@
-alert("geladen");
 var aktuellesBoot;
 $(document).ready(function() {
 	loadBootInfo();
@@ -8,47 +7,53 @@ $(document).ready(function() {
 			type : "POST",
 			url : "saveBoot.jsp",
 			data : fields,
-			success:loadBootInfo()
+			success: function(){
+				loadBootInfo();
+			}
 		});
-		
 	});
-//	$(".boot").click(function(){
-//		alert("funtioniert");
-//		$(this).css({
-//			"background-color": "blue"
-//		  });
-//	});
 });
 function loadBootInfo() {
 	$("#Boottabelle").empty();
-	$("#Boottabelle").load("BootInfo.jsp");
-}
-function showBoot(aktuellesBoot){
-	
-}
-function resetForm(){
-	$('#registernr').value="";
-	$('#bootsname').value="";
-	$('#segelzeichen').value="";
-	$('#heimathafen').value="";
-	$('#yachtclub').value="";
-	$('#eigner').value="";
-	$('#versicherung').value="";
-	$('#rufzeichen').value="";
-	$('#typ').value="";
-	$('#konstrukteur').value="";
-	$('#laenge').value="";
-	$('#breite').value="";
-	$('#tiefgang').value="";
-	$('#masthoehe').value="";
-	$('#verdraengung').value="";
-	$('#rigArt').value="";
-	$('#baujahr').value="";
-	$('#motor').value="";
-	$('#tankgroesse').value="";
-	$('#wassertankgroesse').value="";
-	$('#abwassertankgroesse').value="";
-	$('#grosssegelgroesse').value="";
-	$('#genuagroesse').value="";
-	$('#spi').value="";
+	$("#Boottabelle").load("BootInfo.jsp", function jspReady(){
+		
+		$(".row").mouseover(function(){
+			$(this).css({
+				"background-color": "grey"
+			  });
+		});
+		$(".row").mouseleave(function(){
+			$(this).css({
+				"background-color": "white"
+			  });
+		});
+		$(".row").click(function(){
+			set_values($(this).get());
+//			var boot = $(this).text();
+//			document.getElementById("Bootsname").value = boot[0].innerHTML;
+//			document.getElementById("registernr").value = boot[1].innerHTML;
+//			document.getElementById("segelzeichen").value = boot[2].innerHTML;
+//			document.getElementById("heimathafen").value = boot[3].innerHTML;
+//			document.getElementById("yachtclub").value = boot[4].innerHTML;
+//			document.getElementById("eigner").value = boot[5].innerHTML;
+//			document.getElementById("versicherung").value = boot[6].innerHTML;
+//			document.getElementById("rufzeichen").value = boot[7].innerHTML;
+//			document.getElementById("typ").value = boot[8].innerHTML;
+//			document.getElementById("konstrukteur").value = boot[9].innerHTML;
+//			document.getElementById("laenge").value = boot[10].innerHTML;
+//			document.getElementById("breite").value = boot[11].innerHTML;
+//			document.getElementById("tiefgang").value = boot[12].innerHTML;
+//			document.getElementById("masthoehe").value = boot[13].innerHTML;
+//			document.getElementById("verdraengung").value = boot[14].innerHTML;
+//			document.getElementById("rigArt").value = boot[15].innerHTML;
+//			document.getElementById("baujahr").value = boot[16].innerHTML;
+//			document.getElementById("motor").value = boot[17].innerHTML;
+//			document.getElementById("tankgroesse").value = boot[18].innerHTML;
+//			document.getElementById("wassertankgroesse").value = boot[19].innerHTML;
+//			document.getElementById("abwassertankgroesse").value = boot[20].innerHTML;
+//			document.getElementById("grosssegelgroesse").value = boot[21].innerHTML;
+//			document.getElementById("genuagroesse").value = boot[22].innerHTML;
+//			document.getElementById("spi").value = boot[23].innerHTML;	
+		});
+	});
 }
